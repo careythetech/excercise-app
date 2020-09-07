9fb9e3ff118fd95586cc4860d159d091f140589f
Careytech <careythetech@gmail.com>
2020-09-06 22:00:21 -0400

    Made Changes

--- models/Exercise.js
+++ models/Exercise.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
-
-
 const exerciseSchema = new mongoose.Schema({
     username: {
         type: String,
--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config;
+require('dotenv').config();
 const app = express();
 app.use(cors());
 
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
 app.use(express.static(`${__dirname}/client/build`));
 
 app.get('/*', (req, res) => {
     res.sendFile(`${__dirname}/client/build/index.html`)
 });
 
-const uri = process.env.ATLAS_URI || "mongodb://localhost/mern_excercise";
-mongoose.connect(uri, { useNewUrlParser: true}, { useUnifiedTopology: true })
-.then(() => {
-    console.log("connected to mongo at: " + uri);
-});
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex:true} 
+    );
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('MongoDB Connected');
+    })
+
 
 const router = require('./routes/exercises');
 // const usersRouter = require('./routes/users');
@@ -26,7 +30,7 @@ const router = require('./routes/exercises');
 app.use('/api/exercises/', router);
 // app.use('./users', usersRouter);
 
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 });
