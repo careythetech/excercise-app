76d8c238389e8b79bf26aa28bd4a51b93429d1e2
Careytech <careythetech@gmail.com>
2020-10-20 12:37:50 -0400

    made build file

---
+++ client/build/precache-manifest.72f1cab156cbe4bb9a85b71eff31e59b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "804debd5247e6c985b0be23b6bed803c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56a5892b9f06f0b0506f",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "79ffdf03986699d281cf",
+    "url": "/static/css/main.498c16e2.chunk.css"
+  },
+  {
+    "revision": "56a5892b9f06f0b0506f",
+    "url": "/static/js/2.c3a2705d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c3a2705d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79ffdf03986699d281cf",
+    "url": "/static/js/main.f9141811.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.68ccfaf0522b0adb06dd1d2ac9a3a6b8.js"
+  "/precache-manifest.72f1cab156cbe4bb9a85b71eff31e59b.js"
 );
 
 self.addEventListener('message', (event) => {
